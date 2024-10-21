7bf618e42b809a2babb2c74dbb5cf1eb0ead7f8f
SofiaBritps <sofiibritos01@gmail.com>
2024-10-21 16:30:43 -0300

    front: (feature) Dashboard and navbar created

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MonitoreoGloblaComponent } from './monitoreo-globla/monitoreo-globla.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MonitoreoGlobalComponent } from './monitoreo-global/monitoreo-global.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MonitoreoGloblaComponent,
+    NavbarComponent,
+    MonitoreoGlobalComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
