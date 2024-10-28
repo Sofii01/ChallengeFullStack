097842cf0a4fa134faa417b67be100b78662d6e7
SofiaBritps <sofiibritos01@gmail.com>
2024-10-27 21:04:33 -0300

    back: (fix) Fix allowed

--- back/Challenge/src/main/java/com/challenge/Challenge/config/SecurityConfig.java
+++ back/Challenge/src/main/java/com/challenge/Challenge/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://challenge-full-stack.vercel.app/"));
+        configuration.setAllowedOrigins(Arrays.asList("https://challenge-full-stack.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
         configuration.setAllowCredentials(false);
